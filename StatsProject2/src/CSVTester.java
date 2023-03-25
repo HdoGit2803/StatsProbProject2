7926f77590eca716ff8d4bd44a408e275cedc46a
hdo28 <hdo28@10.0.0.85>
2023-03-25 15:50:24 -0400

    CSV changes

--- StatsProject2/src/CSVTester.java
+++ StatsProject2/src/CSVTester.java
@@ -1,33 +1,29 @@
 import java.io.*;
-import java.util.Scanner;
 public class CSVTester
 {
 
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in);
 		MakeCSV def = new MakeCSV();
 		HoldCSV norm = new HoldCSV();
 		File stat = new File("stat.CSV");
-		int range;
-		int bump;
-		int windowValue;
-		int interval;
-		System.out.println("Enter the starting index from -100 to 100");
-		range = sc.nextInt();
-		System.out.println("Enter the range the salter will minus or add by 0 or greater");
-		bump = sc.nextInt();
-		System.out.println("Enter the window value 1 or greater");
-		windowValue = sc.nextInt();
-		System.out.println("Enter the interval of the plotted points 1 or greater");
-		interval = sc.nextInt();
+		
+		//range of csv from x to 100 where x is between -100 to 100
+		int range = 0;
+		//random generated number that the salter will salt the graph by
+		int bump = 3;
+		//the window value for the smoother
+		int windowValue = 3;
+		//interval in which the points will be plotted
+		int interval = 1;
+
 		
 		def.output(range,interval);
 		norm.ReadFile(stat);
 		def.salter(stat, bump);
 		def.smoother(stat, windowValue,range);
-		//def.smoother(stat, windowValue,range);
-		//def.smoother(stat, windowValue,range);
+		def.smoother(stat, windowValue,range);
+		def.smoother(stat, windowValue,range);
 		System.out.println();
 
 	}
